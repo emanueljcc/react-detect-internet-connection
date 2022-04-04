341cec0d1cd1623e43aa68d5485ef23911955759
Emanuel Castillo <emanuelcastillo9101@gmail.com>
2022-04-04 15:48:33 -0400

    Updates

---
+++ precache-manifest.0cb73378cf04043d18de0c823d29ba9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57680265e380e8b15bc72039256f05ef",
+    "url": "./index.html"
+  },
+  {
+    "revision": "77ded478f146a28b1ec1",
+    "url": "./static/css/main.d5ab150a.chunk.css"
+  },
+  {
+    "revision": "480733971eb3d2f4e328",
+    "url": "./static/js/2.ff3f6748.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ff3f6748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ded478f146a28b1ec1",
+    "url": "./static/js/main.084fffa7.chunk.js"
+  },
+  {
+    "revision": "3f3628657757a6010d34",
+    "url": "./static/js/runtime-main.265a6f3e.js"
+  }
+]);
